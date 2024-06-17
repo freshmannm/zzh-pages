47d76b2b39f639bfdd2ffe324b277e76c7eca11e
渣渣辉 <llh7037@163.com>
2024-06-17 08:37:58 +0800

    init

---
+++ docBuild/.vuepress/config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    title: '渣渣辉记录文档',// 设置网站标题
+    description: '文档',
+    base: '/zzh-pages/',   // 设置站点根路径
+    dest: './docs',  // 设置输出目录
+    port: 8086,
+    head: [], // 增加一个自定义的 favicon // 增加一个自定义的 favicon],
+    plugins: [],
+    themeConfig: {
+        // 添加导航栏
+        nav: [
+            {text: 'home', link: '/'},
+            {text: '日常', link: '/day/notes'},
+        ],
+        // 为以下路由添加左侧边栏
+        sidebar: {
+            '/day/': [
+                {
+                    title: '记录笔记',
+                    collapsable: false,
+                    children: [
+                        {title: '笔记', path: '/day/notes'},
+                        {title: '代码块', path: '/day/codeBlocks'},
+                    ]
+                }
+            ],
+        },
+        sidebarDepth: 1,//左侧导航显示的层级
+        lastUpdated: 'Last Updated'
+    }
+}
